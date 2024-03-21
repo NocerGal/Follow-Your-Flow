604b7f213deec1ff31dab3272624f6c70d899474
NocerGal <luc.schenherr@hotmail.fr>
2024-03-21 11:17:24 +0100

    Création du fichier regroupant nos différents providers

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,7 +1,20 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
+import { ThemeProvider } from 'next-themes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { TailwindIndicator } from '@/src/TailwindIndicator';
 
 export default function Providers({ children }: PropsWithChildren) {
-  return <SessionProvider>{children}</SessionProvider>;
+  const queryClient = new QueryClient();
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <SessionProvider>
+        <TailwindIndicator />
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </SessionProvider>
+    </ThemeProvider>
+  );
 }

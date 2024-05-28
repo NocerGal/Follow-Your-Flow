0fcadb4f240c991a2cdc37acf350d70bab6a451b
NocerGal <luc.schenherr@hotmail.fr>
2024-05-28 08:08:33 +0200

    Modification de nom de fonction

--- app/flow/new/user.query.ts
+++ app/flow/new/user.query.ts
@@ -1,18 +1,26 @@
 'use server';
 import { prisma } from '@/src/lib/prisma';
 
-export const queryUserIdByUserName = async (userName: string) => {
+const queryUserId = async (userName: string) => {
   const userId = await prisma.user.findFirst({
+    where: { name: userName },
+  });
+  return userId!.id;
+};
+
+export const queryUserByUserName = async (userName: string) => {
+  const user = await prisma.user.findFirst({
     where: {
       name: userName,
     },
-    select: {
-      id: true,
-      name: true,
-    },
   });
 
-  return userId;
+  if (!user) {
+    throw new Error(`User with username ${userName} not found`);
+  }
+  const userId = await queryUserId(userName);
+
+  return { user, userId };
 };
 
 export const queryManagerOnStep = async (stepId: string) => {
